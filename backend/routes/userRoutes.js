c949f9d8a1c5863f1b44fe3d93a50f390528c36f
Espadv69 <aitorjob69@gmail.com>
2025-03-28 19:07:47 +0100

    Add routes for user management: create new user and retrieve all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import { addUser, getUsers } from '../controllers/userController.js'
 
 const API_USERS = '/api/users'
 const API_USERS_ID = '/api/users/:id'
 
 const router = express.Router()
+
+// Route to add a new user 🟢
+router.post(API_USERS, addUser)
+
+// Route to get all users 🔍
+router.get(API_USERS, getUsers)
 
 export default router
