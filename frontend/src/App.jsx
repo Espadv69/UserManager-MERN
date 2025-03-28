f8386f2ab517433632f81f5eff67ce521775b2c1
Espadv69 <aitorjob69@gmail.com>
2025-03-28 19:45:47 +0100

    Add routing setup in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,19 @@
-const App = () => {}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Paths
+const USER_LIST = '/'
+const USER_CREATE = '/create'
+const EXPLANATION = '/explanation'
+
+const App = () => {
+  return (
+    <Router>
+      {/* Add NavBar */}
+      <Routes>
+        <Route path={USER_LIST} element />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
