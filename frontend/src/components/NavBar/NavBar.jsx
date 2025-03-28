9261122e959ba476abe3255aea95ea510394a339
Espadv69 <aitorjob69@gmail.com>
2025-03-28 19:56:58 +0100

    Add navigation links to NavBar component for user list, add user, and explanation

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,25 @@
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 
-const NavBar = () => {}
+// Paths
+const USER_LIST = '/'
+const USER_CREATE = '/create'
+const EXPLANATION = '/explanation'
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <Link to={USER_LIST} className="nav-link">
+        User List
+      </Link>
+      <Link to={USER_CREATE} className="nav-link">
+        Add User
+      </Link>
+      <Link to={EXPLANATION} className="nav-link">
+        Explanation
+      </Link>
+    </nav>
+  )
+}
 
 export default NavBar
