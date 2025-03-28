05436f3e1b6aa584b2a0dba26bbce179d6b768d8
Espadv69 <aitorjob69@gmail.com>
2025-03-28 18:46:57 +0100

    Add user routes for API endpoints

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const WELCOME = '/'
+const API_USERS = '/api/users'
+const API_USERS_ID = '/api/users/:id'
+
+const router = express.Router()
+
+export default router
