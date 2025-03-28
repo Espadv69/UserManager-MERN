ee8a4c38b487d07ee30e984c504c5d34bed6e89b
Espadv69 <aitorjob69@gmail.com>
2025-03-28 18:50:51 +0100

    Add user model schema with validation for user attributes

---
+++ backend/model/User.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  identificationNumber: { type: String, required: true, unique: true },
+})
