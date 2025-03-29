69686e4a5fdff665c1dc81dd0eadcb2fb1db5ccc
Espadv69 <aitorjob69@gmail.com>
2025-03-29 12:47:40 +0100

    Enhance UserFormInput component to accept props for label, type, value, setter, and placeholder

--- frontend/src/components/UserForm/UserFormInput.jsx
+++ frontend/src/components/UserForm/UserFormInput.jsx
@@ -1,3 +1,22 @@
-const UserFormInput = () => {}
+const UserFormInput = ({
+  labelName,
+  inputType,
+  inputValue,
+  setValue,
+  inputPlaceholder,
+}) => {
+  return (
+    <label className="user-form__label">
+      {labelName}
+      <input
+        type={inputType}
+        value={inputValue}
+        className="user-form__input"
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={inputPlaceholder}
+      />
+    </label>
+  )
+}
 
 export default UserFormInput
