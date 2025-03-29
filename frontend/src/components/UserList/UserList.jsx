04fcaed76289e670fc8880261f78b49bcf861966
Espadv69 <aitorjob69@gmail.com>
2025-03-29 17:35:18 +0100

    Add state management for users and loading in UserList component

--- frontend/src/components/UserList/UserList.jsx
+++ frontend/src/components/UserList/UserList.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { API_URL } from '../../utils/const.js'
+
 import './UserList.css'
 
-const UserList = () => {}
+const UserList = () => {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+}
 
 export default UserList
