6d56cbea3a79f4becaf52c43669c57d2673e453a
Espadv69 <aitorjob69@gmail.com>
2025-03-29 12:02:04 +0100

    Implement user addition functionality in UserForm component

--- frontend/src/components/UserForm/UserForm.jsx
+++ frontend/src/components/UserForm/UserForm.jsx
@@ -7,6 +7,21 @@ const UserForm = () => {
   const [email, setEmail] = useState('')
   const [identificationNumber, setIdentificationNumber] = useState('')
   // const [error, setError] = useState('') // Todo: Add error handling
+
+  // Add user to the database 📗
+  const addUser = async () => {
+    // Todo: Add error handling
+    // Stop if any field is empty
+    if (!firstName || !lastName || !email || !identificationNumber) return
+
+    // Create a new user object 🆕
+    const newUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      identificationNumber: identificationNumber,
+    }
+  }
 }
 
 export default UserForm
