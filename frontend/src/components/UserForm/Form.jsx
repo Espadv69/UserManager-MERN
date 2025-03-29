40bea7435f9cdb0b6710a2aac6fb696989513b53
Espadv69 <aitorjob69@gmail.com>
2025-03-29 12:59:46 +0100

    Implement UserForm component to handle user input with multiple fields

--- frontend/src/components/UserForm/Form.jsx
+++ frontend/src/components/UserForm/Form.jsx
@@ -1,3 +1,53 @@
-const Form = () => {}
+import UserFormInput from './UserFormInput'
+
+const Form = ({
+  onSubmit,
+  firstName,
+  setFirstName,
+  lastName,
+  setLastName,
+  email,
+  setEmail,
+  identificationNumber,
+  setIdentificationNumber,
+}) => {
+  return (
+    <form className="user-form" onSubmit={onSubmit}>
+      <UserFormInput
+        labelName="First Name:"
+        inputType="text"
+        inputValue={firstName}
+        setValue={setFirstName}
+        inputPlaceholder="Enter First Name"
+      />
+
+      <UserFormInput
+        labelName="Last Name:"
+        inputType="text"
+        inputValue={lastName}
+        setValue={setLastName}
+        inputPlaceholder="Enter Last Name"
+      />
+
+      <UserFormInput
+        labelName="Email:"
+        inputType="email"
+        inputValue={email}
+        setValue={setEmail}
+        inputPlaceholder="Enter Email"
+      />
+
+      <UserFormInput
+        labelName="Identification Number:"
+        inputType="text"
+        inputValue={identificationNumber}
+        setValue={setIdentificationNumber}
+        inputPlaceholder="Enter Identification Number"
+      />
+
+      <button className="user-form__button">Add User</button>
+    </form>
+  )
+}
 
 export default Form
