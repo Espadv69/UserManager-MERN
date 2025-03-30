cf027a358aa244028155b3e1a80d439c1f61ceb2
Espadv69 <aitorjob69@gmail.com>
2025-03-30 13:31:49 +0200

    Enhance NavBar layout by removing unused Explanation link and adding a title

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -4,20 +4,19 @@ import './NavBar.css'
 // Paths
 const USER_LIST = '/'
 const USER_CREATE = '/create'
-const EXPLANATION = '/explanation'
 
 const NavBar = () => {
   return (
     <nav className="nav">
-      <Link to={USER_LIST} className="nav-link">
-        User List
-      </Link>
-      <Link to={USER_CREATE} className="nav-link">
-        Add User
-      </Link>
-      <Link to={EXPLANATION} className="nav-link">
-        Explanation
-      </Link>
+      <h2>UserManager 🍉</h2>
+      <div className="nav-links">
+        <Link to={USER_LIST} className="nav-link">
+          User List
+        </Link>
+        <Link to={USER_CREATE} className="nav-link">
+          Add User
+        </Link>
+      </div>
     </nav>
   )
 }
