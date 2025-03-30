28d1af3532c7a7fb31d8eee3277bf8c52e780b51
Espadv69 <aitorjob69@gmail.com>
2025-03-30 13:31:35 +0200

    Remove unused Explanation route from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import UserListPage from './pages/UserListPage'
 import AddUserPage from './pages/AddUser'
-import ExplanationPage from './pages/Explanation'
 
 // Paths
 const USER_LIST = '/'
 const USER_CREATE = '/create'
-const EXPLANATION = '/explanation'
 
 const App = () => {
   return (
@@ -17,7 +15,6 @@ const App = () => {
       <Routes>
         <Route path={USER_LIST} element={<UserListPage />} />
         <Route path={USER_CREATE} element={<AddUserPage />} />
-        <Route path={EXPLANATION} element={<ExplanationPage />} />
       </Routes>
     </Router>
   )
