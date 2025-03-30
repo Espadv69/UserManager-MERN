ba58af45452b164d89d85aa006124f23bae50316
Espadv69 <aitorjob69@gmail.com>
2025-03-30 12:30:58 +0200

    Add route to delete a user by ID in userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addUser, getUsers } from '../controllers/userController.js'
+import { addUser, getUsers, deleteUser } from '../controllers/userController.js'
 
 const API_USERS = '/users'
 const API_USERS_ID = '/users/:id'
@@ -11,5 +11,8 @@ router.post(API_USERS, addUser)
 
 // Route to get all users 🔍
 router.get(API_USERS, getUsers)
+
+// Route to delete a user by ID 🗑️
+router.delete(API_USERS_ID, deleteUser)
 
 export default router
